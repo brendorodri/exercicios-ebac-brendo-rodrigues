35074943e9ef03dca10e1cd3a78053717c9e7a55
brendorodri <brendorodriguesdebrito@gmail.com>
2023-08-15 21:38:00 -0300

    Exercicio Modulo 09

---
+++ tarefa-wrapper/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author brendorodri
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        long cpf = 327891298;
+        System.out.println(cpf);
+
+        Long cpfWrapper = cpf;
+        System.out.println(cpfWrapper);
+    }
+}
