a0f2e58ec7f1cfc6e03c5d015d199f5ba4510f51
brendorodri <brendorodriguesdebrito@gmail.com>
2023-08-25 22:10:53 -0300

    Alterando texto dos Scanners

--- tarefa-controle-de-fluxo/src/br/com/ebac/CalculoNota.java
+++ tarefa-controle-de-fluxo/src/br/com/ebac/CalculoNota.java
@@ -11,20 +11,21 @@ public class CalculoNota {
         Nota geografia = new Nota();
         Nota historia = new Nota();
 
-        System.out.println("Digite sua nota portugues: ");
+        System.out.println("Digite sua nota de Portugues: ");
         portugues.inserirMinhaNota(s.nextDouble());
 
-        System.out.println("Digite sua nota matematica: ");
+        System.out.println("Digite sua nota de Matematica: ");
         matematica.inserirMinhaNota(s.nextDouble());
 
-        System.out.println("Digite sua nota historia: ");
+        System.out.println("Digite sua nota de Historia: ");
         historia.inserirMinhaNota(s.nextDouble());
 
-        System.out.println("Digite sua nota geografia: ");
+        System.out.println("Digite sua nota de Geografia: ");
         geografia.inserirMinhaNota(s.nextDouble());
 
         Double resultado = (matematica.getMinhaNota() + portugues.getMinhaNota() + historia.getMinhaNota() + geografia.getMinhaNota()) / 4;
 
+        System.out.println("Sua média é igual: " + resultado);
         System.out.println("Resultado: " + getNota(resultado));
     }
 
