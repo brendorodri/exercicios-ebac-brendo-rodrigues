7ed039b91aadabd6eb532b382081badf0f9a1762
brendorodri <brendorodriguesdebrito@gmail.com>
2023-08-26 13:50:23 -0300

    Exercicio Modulo 10 - Controle de Fluxo

--- tarefa-controle-de-fluxo/src/br/com/ebac/CalculoNota.java
+++ tarefa-controle-de-fluxo/src/br/com/ebac/CalculoNota.java
@@ -4,28 +4,29 @@ import java.util.Scanner;
 
 public class CalculoNota {
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
 
-        Nota portugues = new Nota();
-        Nota matematica = new Nota();
-        Nota geografia = new Nota();
-        Nota historia = new Nota();
+        Disciplina primeiraDisciplina = new Disciplina();
+        Disciplina segundaDisciplina = new Disciplina();
+        Disciplina terceiraNota = new Disciplina();
+        Disciplina quartaDisciplina = new Disciplina();
 
-        System.out.println("Digite sua nota de Portugues: ");
-        portugues.inserirMinhaNota(s.nextDouble());
+        System.out.println("Digite sua primeira nota: ");
+        primeiraDisciplina.inserirMinhaNota(s.nextDouble());
 
-        System.out.println("Digite sua nota de Matematica: ");
-        matematica.inserirMinhaNota(s.nextDouble());
+        System.out.println("Digite sua segunda nota: ");
+        segundaDisciplina.inserirMinhaNota(s.nextDouble());
 
-        System.out.println("Digite sua nota de Historia: ");
-        historia.inserirMinhaNota(s.nextDouble());
+        System.out.println("Digite sua terceira nota : ");
+        terceiraNota.inserirMinhaNota(s.nextDouble());
 
-        System.out.println("Digite sua nota de Geografia: ");
-        geografia.inserirMinhaNota(s.nextDouble());
+        System.out.println("Digite sua quarta nota: ");
+        quartaDisciplina.inserirMinhaNota(s.nextDouble());
 
-        Double resultado = (matematica.getMinhaNota() + portugues.getMinhaNota() + historia.getMinhaNota() + geografia.getMinhaNota()) / 4;
+        Double resultado = (primeiraDisciplina.getMinhaNota() + segundaDisciplina.getMinhaNota() + terceiraNota.getMinhaNota() + quartaDisciplina.getMinhaNota()) / 4;
 
-        System.out.println("Sua média é igual: " + resultado);
+        System.out.println("Sua média é: " + resultado);
         System.out.println("Resultado: " + getNota(resultado));
     }
 
